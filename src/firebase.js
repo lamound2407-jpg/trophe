e27fb2060ba47db28f26cc5a91621b179b45f019
lamound2407-jpg <lamound2407@gmail.com>
2026-08-16 01:35:57 -0500

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInAnonymously } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDAmQHamSBBShD-lwhVdH3VtGiOVH21U88",
+  authDomain: "trophe-4974e.firebaseapp.com",
+  projectId: "trophe-4974e",
+  storageBucket: "trophe-4974e.firebasestorage.app",
+  messagingSenderId: "371454645448",
+  appId: "1:371454645448:web:76193d1dd8a7ac2c7801fd"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export async function signIntoTrophe() {
+  if (!auth.currentUser) {
+    await signInAnonymously(auth);
+  }
+
+  return auth.currentUser;
+}
